545333c8925140dd9ba7a1349b9273fa96caef0b
pathologyenigma <pathology666@163.com>
2020-12-22 09:44:14 +0800

    add material for every grid

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,22 @@ fn hello(commands:&mut Commands) {
 fn create_board(commands:&mut Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>) {
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane {size:8.0})),
-        material: materials.add(Color::rgb(1.,0.9,0.9).into()),
-        transform: Transform::from_translation(Vec3::new(4.,0.,4.)),
-        ..Default::default()
-    });
+        let mesh = meshes.add(Mesh::from(shape::Plane {size: 1.}));
+        let white_material = materials.add(Color::rgb(1.,0.9,0.9).into());
+        let black_material = materials.add(Color::rgb(0.,0.1,0.1).into());
+        for i in 0..8 {
+            for j in 0..8{
+                commands.spawn(PbrBundle {
+                    mesh: mesh.clone(),
+                    material: if (i + j + 1) % 2 == 0 {
+                        white_material.clone()
+                    } else {
+                        black_material.clone()
+                    },
+                    transform: Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
+                    ..Default::default()
+                });
+            }
+        }
+        
 }
