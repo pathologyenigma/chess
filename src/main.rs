2b0e3a1b3a15bcf06ce9af80b19e63c17a060337
pathologyenigma <pathology666@163.com>
2020-12-22 09:12:51 +0800

    add a new system called create_board

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main(){
     })
     .add_plugins(DefaultPlugins)
     .add_startup_system(hello.system())
+    .add_startup_system(create_board.system())
     .run();
 }
 
@@ -26,6 +27,21 @@ fn hello(commands:&mut Commands,
     .spawn(Camera3dBundle {
         transform: Transform::from_matrix(Mat4::from_rotation_translation(
             Quat::from_xyzw(-0.3,-0.5,-0.3,0.5).normalize(),
+            Vec3::new(-7.0,20.0,4.0),
+        )),
+        ..Default::default()
+    })
+    .spawn(LightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0,8.0,4.0)),
+        ..Default::default()
+    });
+}
+
+fn create_board(commands:&mut Commands) {
+    commands
+    .spawn(Camera3dBundle {
+        transform: Transform::from_matrix(Mat4::from_rotation_translation(
+            Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5),
             Vec3::new(-7.0,20.0,4.0),
         )),
         ..Default::default()
